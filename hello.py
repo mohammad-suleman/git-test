e236895ccc3b6663c9bdabc27f6776ae6b193371
mohammad-suleman <muhammadsulemankhan123@gmail.com>
2024-06-22 12:51:36 +0500

    first python code

---
+++ hello.py
@@ -0,0 +1,20 @@
+# If-else statements
+x = 15
+
+if x < 10:
+    print("x is less than 10")
+elif x == 10:
+    print("x is 10")
+else:
+    print("x is greater than 10")
+
+# For loop
+for i in range(5):
+    print(i)
+
+# While loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
